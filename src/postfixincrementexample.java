b6317265023c6b30c2a17e4d71463c06ecce86c9
Rikimo23 <162639270+Rikimo23@users.noreply.github.com>
2024-07-03 12:13:34 -0400

    postincrementexample

---
+++ src/postfixincrementexample.java
@@ -0,0 +1,17 @@
+public class postfixincrementexample {
+
+    public static void main(String[] args) {
+        // Declare and assign a number to an integer variable
+        int number = 5;
+
+        // Print the initial value of the variable
+        System.out.println("Initial value: " + number);
+
+        // Use postfix increment operator to increase the value
+        number++;
+
+        // Print the value after using the postfix increment operator
+        System.out.println("Value after increment: " + number);
+    }
+
+}
